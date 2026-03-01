d628b0219d440711a5f9429a7d973b16b4a2e0da
oberlhu <oberlhu@tencent.com>
2026-03-01 16:18:24 +0800

    refactor: 移除未使用的组件和代码

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,15 +1,6 @@
 <template>
   <!-- 基本信息 -->
   <div class="message">
-    <!-- Logo -->
-    <div class="logo">
-      <img class="logo-img" :src="siteLogo" alt="logo" />
-      <div :class="{ name: true, 'text-hidden': true, long: siteUrl[0].length >= 6 }">
-        <span class="bg">{{ siteUrl[0] }}</span>
-        <span class="sm">.{{ siteUrl[1] }}</span>
-      </div>
-    </div>
-    <!-- 简介 -->
     <div class="description cards" @click="changeBox">
       <div class="content">
         <Icon size="16">
@@ -35,20 +26,6 @@ import { QuoteLeft, QuoteRight } from "@vicons/fa";
 import { Error } from "@icon-park/vue-next";
 import { mainStore } from "@/store";
 const store = mainStore();
-
-// 主页站点logo
-const siteLogo = import.meta.env.VITE_SITE_MAIN_LOGO;
-// 站点链接
-const siteUrl = computed(() => {
-  const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "hackber.com".split(".");
-  // 判断协议前缀
-  if (url.startsWith("http://") || url.startsWith("https://")) {
-    const urlFormat = url.replace(/^(https?:\/\/)/, "");
-    return urlFormat.split(".");
-  }
-  return url.split(".");
-});
 
 // 简介区域文字
 const descriptionText = reactive({
@@ -89,54 +66,9 @@ watch(
 
 <style lang="scss" scoped>
 .message {
-  .logo {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    animation: fade 0.5s;
-    max-width: 460px;
-    .logo-img {
-      border-radius: 50%;
-      width: 120px;
-    }
-    .name {
-      width: 100%;
-      padding-left: 22px;
-      transform: translateY(-8px);
-      font-family: "Pacifico-Regular";
-
-      .bg {
-        font-size: 5rem;
-      }
-
-      .sm {
-        margin-left: 6px;
-        font-size: 2rem;
-        @media (min-width: 721px) and (max-width: 789px) {
-          display: none;
-        }
-      }
-    }
-    @media (max-width: 768px) {
-      .logo-img {
-        width: 100px;
-      }
-      .name {
-        height: 128px;
-        .bg {
-          font-size: 4.5rem;
-        }
-      }
-    }
-
-    @media (max-width: 720px) {
-      max-width: 100%;
-    }
-  }
-
   .description {
     padding: 1rem;
-    margin-top: 3.5rem;
+    margin-top: 0;
     max-width: 460px;
     animation: fade 0.5s;
 
